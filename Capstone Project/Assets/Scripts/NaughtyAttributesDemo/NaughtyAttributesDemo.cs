f903021204e9a517b54516feeec1a38895f180e6
Lokust001 <Lokusths@gmail.com>
2025-09-18 13:29:30 -0500

    Re-imported naughtyattributes
    reimported naughtyattributes and made some headway on the naughtyattributes demo

--- Capstone Project/Assets/Scripts/NaughtyAttributesDemo/NaughtyAttributesDemo.cs
+++ Capstone Project/Assets/Scripts/NaughtyAttributesDemo/NaughtyAttributesDemo.cs
@@ -1,5 +1,7 @@
-using UnityEngine;
 using NaughtyAttributes;
+
+using System.Collections.Generic;
+using UnityEngine;
 
 
 public class NaughtyAttributesDemo : MonoBehaviour
@@ -7,15 +9,47 @@ public class NaughtyAttributesDemo : MonoBehaviour
     [System.Serializable] public enum Settings 
     {
         WeaponSettings,
-        ArmorSettings,
+        PlayerSettings,
         OtherSettings
     }
 
-    
-
     [SerializeField] private Settings currentInspectorShowing;
 
-    private bool test;
+    [Space(4)]
+
+    [ShowIf(nameof(currentInspectorShowing), Settings.WeaponSettings), SerializeField]
+    private bool weaponPower;
+
+    [Space(4)]
+
+    [ShowIf(nameof(currentInspectorShowing), Settings.PlayerSettings), SerializeField]
+    private float playerSpeed;
+
+    [Space(4)]
+
+    [ShowIf(nameof(currentInspectorShowing), Settings.PlayerSettings), SerializeField, MinMaxSlider(0, 100)]
+    private Vector2Int playerMaxHealth;
+
+    [Space(4)]
+
+    [ShowIf(nameof(currentInspectorShowing), Settings.OtherSettings), SerializeField, InfoBox("Do Not Mess With This Please", EInfoBoxType.Warning)]
+    private string errorOutput;
+
+    [Space(4)]
+    [ShowIf(nameof(currentInspectorShowing), Settings.OtherSettings), SerializeField, Dropdown(nameof(stringVals))]
+    private string animalName;
+
+
+
+    [HorizontalLine(3, EColor.Green)]
+    [SerializeField, Tag] private string TagToLookFor;
+
+
+    [Foldout("GeneralSettings"), SerializeField] private string objName;
+    [Foldout("GeneralSettings"), SerializeField] private string objDiscription;
+
+
+    private List<string> stringVals = new List<string>() { "dog", "cat", "hawk" };
 
 
 }
