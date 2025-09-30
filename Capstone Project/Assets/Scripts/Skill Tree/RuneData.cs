877db7917a5fe6bac3eee97f3e25e6ef4c6dd68b
Lokust001 <Lokusths@gmail.com>
2025-09-30 11:17:16 -0500

    Added Rune Data and removed unused scripts
    - removed the clickable script (unused now)
    
    - added rune data container class so it can be used both in and out of the skill tree

---
+++ Capstone Project/Assets/Scripts/Skill Tree/RuneData.cs
@@ -0,0 +1,36 @@
+/*************************************************
+Author Names : 		Tyler Hayes 
+Date Created : 		09/30/2025
+Date Last Modified : 09/30/2025
+Brief Description : This is the data container for what nodes have
+                    been unlocked on the skill tree
+External Resources : 	
+	***************************************************/
+
+using UnityEngine;
+
+//this is the general enum for the rune type
+public enum RuneType
+{
+    Lightning,
+    Wind
+}
+public class RuneData
+{
+    //stores the type of rune
+    public RuneType typeOfRune;
+
+    //stores which version of the element it is
+    public int numberOnSkillTree;
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="typeOfRune"> The element of rune this is </param>
+    /// <param name="numberOnSkillTree"> which version of the element it is </param>
+    public RuneData(RuneType typeOfRune, int numberOnSkillTree)
+    {
+        this.typeOfRune = typeOfRune;
+        this.numberOnSkillTree = numberOnSkillTree;
+    }
+}
