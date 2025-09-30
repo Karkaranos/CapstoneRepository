2e8f5d567f524e73a84da413d258ae37e0dadbe7
ClareG2004 <cgrady@mail.bradley.edu>
2025-09-30 11:50:10 -0500

    I always forget to save some scripts

--- Capstone Project/Assets/Scripts/Enemy/Interfaces/IDamageable.cs
+++ Capstone Project/Assets/Scripts/Enemy/Interfaces/IDamageable.cs
@@ -1,6 +1,17 @@
+/*************************************************
+Author Names : 		Clare Grady, 
+Date Created : 		09/30/2025
+Date Last Modified : 	09/30/2025
+Brief Description : 		class description here
+				If more lines needed, spill over to the next
+				And keep indentation
+External Resources : 	list, including a link to the resource used
+	***************************************************/
+
 using UnityEngine;
 
 public interface IDamageable
 {
-
+	
 }
+ 
