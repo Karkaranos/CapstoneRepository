d88ffe22188722ad7a057dd2ee5ccd25cd3c1911
Sleepy2004 <aratcliffe@mail.bradley.edu>
2025-10-03 15:32:23 -0500

    Making UI screens for the player

--- Capstone Project/Assets/Scripts/ButtonManager.cs
+++ Capstone Project/Assets/Scripts/ButtonManager.cs
@@ -1,7 +1,7 @@
 /*************************************************
 Author Names : 		    Aidan Ratcliffe
 Date Created : 		    10/1/2025
-Date Last Modified : 	10/1/2025
+Date Last Modified : 	10/2/2025
 Brief Description : 	All Buttons will be managed within this script
 External Resources : 	N/A
 ***************************************************/
@@ -13,23 +13,28 @@ public class ButtonManager : MonoBehaviour
 {
     public GameObject PlayerCanvas;
     public GameObject MoveCanvas;
+    public GameObject ConfirmCanvas;
     public Button MoveButton;
+    public Button MoveChoiceButton;
     public Button BackButton;
-    //public Button ConfirmButton;
+    public Button ConfirmButton;
     //public Button MoveChoiceButton;
     public bool PlayerCanMove;
     public bool PlayerIsGoingToMove;
     public bool BackButtonClicked;
+    public bool ConfirmButtonClicked;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Button mbtn = MoveButton.GetComponent<Button>();
+        Button mcbtn = MoveChoiceButton.GetComponent<Button>();
         Button bbtn = BackButton.GetComponent<Button>();
-        //Button cbtn = ConfirmButton.GetComponent<Button>();
+        Button cbtn = ConfirmButton.GetComponent<Button>();
         //Button mcbtn = MoveChoiceButton.GetComponent<Button>();
         Canvas playerCanvas = PlayerCanvas.GetComponent<Canvas>();
         Canvas moveCanvas = MoveCanvas.GetComponent<Canvas>();
+        Canvas confirmCanvas = ConfirmCanvas.GetComponent<Canvas>();
     }
 
     public void MoveButtonOnClick()
@@ -62,9 +67,31 @@ public class ButtonManager : MonoBehaviour
         }
     }
 
+    public void MoveChoiceOnClick()
+    {
+        PlayerIsGoingToMove = true;
+        if (PlayerIsGoingToMove)
+        {
+            MoveCanvas.SetActive(false);
+            ConfirmCanvas.SetActive(true);
+        }
+        else
+        {
+            PlayerIsGoingToMove = false;
+        }
+    }
+
     public void ConfirmOnClick()
     {
-
+        ConfirmButtonClicked = true;
+        if (ConfirmButtonClicked)
+        {
+            ConfirmCanvas.SetActive(false);
+        }
+        else
+        {
+            ConfirmButtonClicked = false;
+        }
     }
 
 }
--- Capstone Project/Assets/Scripts/PlayerBehavior.cs
+++ Capstone Project/Assets/Scripts/PlayerBehavior.cs
@@ -1,10 +1,19 @@
+/*************************************************
+Author Names : 		    Aidan Ratcliffe
+Date Created : 		    10/1/2025
+Date Last Modified : 	10/2/2025
+Brief Description : 	This how the player will detect where the grid is
+External Resources : 	N/A
+***************************************************/
 using NUnit.Framework;
 using Unity.VisualScripting;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    private PlayerInput playerInput;
     public GameObject player;
     public GameObject gridTile;
     public List<Vector2Int> gridPoints = new List<Vector2Int>();
@@ -15,6 +24,7 @@ public class PlayerBehavior : MonoBehaviour
     private GridManager gridManager;
     public bool PlayerCanMove;
     public bool PlayerHasMoved;
+    public bool MouseIsClicked;
     //public bool PlayerCanAttack;
     //public bool PlayerHasAttacked;
 
@@ -24,6 +34,7 @@ public class PlayerBehavior : MonoBehaviour
         gridManager = gridManager.gameObject.GetComponent<GridManager>();
         player = player.gameObject.GetComponent<GameObject>();
         tilePosition = tileBehavior.TileIntPosition;
+        playerInput = new PlayerInput();
     }
 
 
@@ -53,5 +64,10 @@ public class PlayerBehavior : MonoBehaviour
         playergridPoints.Add(new Vector2Int(0, 2));
         playergridPoints.Add(new Vector2Int(1, 2));
         playergridPoints.Add(new Vector2Int(1, 3));
+    }
+
+    public void PlayerWillMove()
+    {
+ 
     }
 }
