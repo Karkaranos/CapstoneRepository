15c38c2b5bfe56c70ccb0e4954450f404a19d14d
Karkaranos <karkaranosyt@gmail.com>
2025-10-05 10:41:22 -0500

    Created Artifact Scriptable Object

---
+++ Capstone Project/Assets/Scripts/ScriptableObjectScripts/ArtifactData.cs
@@ -0,0 +1,64 @@
+/*************************************************
+Author Names : 		    Cade Naylor
+Date Created : 		    10/5/2025
+Date Last Modified : 	10/5/2025
+Brief Description : 	Data container for all types of Artifacts, or Equipment                       
+External Resources : 	N/A
+***************************************************/
+using NaughtyAttributes;
+using UnityEngine;
+
+#region Enum Setup
+public enum ArtifactType
+{
+    HAT, CHESTPLATE, BOOT, AMULET, ORB
+}
+
+public enum Effects
+{
+    LIGHTNING_ATK, WIND_ATK, FIRE_ATK, HEALTH, ATTACK, SPEED, ACTION_POINTS, SPELLS_EQUIPPED
+}
+
+#endregion
+
+[CreateAssetMenu(fileName = "ArtifactData", menuName = "ArtifactData")]
+public class ArtifactData : ScriptableObject
+{
+    public string Name;
+    public string Description;
+
+    [Tooltip("The type of Artifact")] public ArtifactType Type;
+    [Tooltip("All effects")] public ArtifactEffects[] Effects;
+    [Tooltip("Takes 1 point away per fight it's used in. Set it to less than 0 to not use this")] public int Durability;
+
+    /// <summary>
+    /// Constructor for ArtifactData in case some Artifacts are generated at runtime
+    /// </summary>
+    /// <param name="name">Name of the Artifact</param>
+    /// <param name="description">Description for the Artifact</param>
+    /// <param name="type">Type of Artifact</param>
+    /// <param name="effects">What stats this affects and their value</param>
+    /// <param name="durability">Optional durability stat. Defaulted paramater sets it to not lose durability</param>
+    public ArtifactData(string name, string description, ArtifactType type, ArtifactEffects[] effects, int durability = -1)
+    {
+        Name = name;
+        Description = description;
+        Type = type;
+        Effects = effects;
+        Durability = durability;
+    }
+
+    /// <summary>
+    /// Copy constructor for Artifact Data
+    /// </summary>
+    /// <param name="ad">Existing Artifact Data</param>
+    public ArtifactData(ArtifactData ad)
+    {
+        Name = ad.Name;
+        Description = ad.Description;
+        Type = ad.Type;
+        Effects = ad.Effects;
+        Durability = ad.Durability;
+    }
+
+}
---
+++ Capstone Project/Assets/Scripts/ScriptableObjectScripts/ArtifactEffects.cs
@@ -0,0 +1,13 @@
+/*************************************************
+Author Names : 		    Cade Naylor
+Date Created : 		    10/5/2025
+Date Last Modified : 	10/5/2025
+Brief Description : 	Helper class for Artifacts                     
+External Resources : 	N/A
+***************************************************/
+[System.Serializable]
+public class ArtifactEffects
+{
+    [AllowNesting] public Effects Effect;
+    [Tooltip("How much the selected stat is changed. Can be positive or negative"), AllowNesting] public float StatChangeAmount;
+}
