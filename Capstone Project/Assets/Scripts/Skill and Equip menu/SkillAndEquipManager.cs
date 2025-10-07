6c468da1df2deabaa16028da46b38689a96d6aac
jayishembryo <kembry@mail.bradley.edu>
2025-10-07 12:36:09 -0500

    New Public Event Added

--- Capstone Project/Assets/Scripts/Skill and Equip menu/SkillAndEquipManager.cs
+++ Capstone Project/Assets/Scripts/Skill and Equip menu/SkillAndEquipManager.cs
@@ -93,6 +93,7 @@ public class SkillAndEquipManager : MonoBehaviour
     public void SetIndexOfEquippedSpells(int index, RuneData data)
     {
         equippedSpells[index] = data;
+        PublicEvents.EquipRunesToCombatMenu(data);
     }
     
     /// <summary>
