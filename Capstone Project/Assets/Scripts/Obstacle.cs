0a3e8a87749a79b4a73ab1afd0b5e25f3d0f5839
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2025-10-20 17:27:36 -0500

    prefab and base code made

---
+++ Capstone Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using NaughtyAttributes;
+
+public class Obstacle : MonoBehaviour
+{
+    public enum ObstacleType {
+        damage,
+        slow,
+        block
+    }
+
+    [SerializeField] public ObstacleType type;
+
+    [SerializeField, ShowIf(nameof(type), ObstacleType.damage)] private float damageAmount;
+    [SerializeField, ShowIf(nameof(type), ObstacleType.slow)] private int turnsRemoved;
+
+
+}
