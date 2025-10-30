f32db980768d07b82a35677a8621858f42481c47
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2025-10-30 10:57:12 -0500

    Enemy healthbar added

--- Capstone Project/Assets/Scripts/Enemy/Base/Enemy.cs
+++ Capstone Project/Assets/Scripts/Enemy/Base/Enemy.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 using NaughtyAttributes;
 using TMPro;
 using Unity.IO.LowLevel.Unsafe;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -29,6 +30,11 @@ public class Enemy : MonoBehaviour
     #region HEALTH VARS
 
     [HorizontalLine(4, EColor.Red)]
+
+    [SerializeField,
+        ShowIf(nameof(currentSettings), Settings.Health),
+        Tooltip("Max health of enemy")]
+    protected Slider healthBarSlider;
 
     [SerializeField, 
         ShowIf(nameof(currentSettings), Settings.Health),
@@ -128,6 +134,7 @@ public class Enemy : MonoBehaviour
     public virtual void Start()
     {
         currentHealth = maxHealth;
+        healthBarSlider.maxValue = maxHealth;
         gridPathfinding = GetComponent<GridPathfinding>();
         targetingBehaviour = GetComponent<TargetingBehaviour>();
         gridPathfinding.SetMovementRange(movementRange);
@@ -144,8 +151,8 @@ public class Enemy : MonoBehaviour
     {
         currentHealth -= damage;
         print("Enemy takes damage");
-
-        if(currentHealth < 0)
+        healthBarSlider.value = currentHealth;
+        if (currentHealth < 0)
         {
             Die();
             if (FindFirstObjectByType<GameManager>().allowArtifacts)
@@ -186,7 +193,6 @@ public class Enemy : MonoBehaviour
             }
         }
     }
-
 
     /// <summary>
     /// Virtual method that all specific enemies will define
