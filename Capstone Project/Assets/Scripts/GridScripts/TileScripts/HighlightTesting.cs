df685c8c3c167818ebd670363f330b197f347a39
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2025-11-22 18:57:57 -0600

    header and comments added

--- Capstone Project/Assets/Scripts/GridScripts/TileScripts/HighlightTesting.cs
+++ Capstone Project/Assets/Scripts/GridScripts/TileScripts/HighlightTesting.cs
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Author: Tyler Bouchard
+ * Creation Date: 11/20/2025
+ * Last Modified: 11/20/2025 (Tyler Bouchard)
+ * Brief: calls highlight finctions in the grid manager to test the tile highlight
+ * External Resources: N/A
+ * ***************************************************************************/
 using UnityEngine;
 using NaughtyAttributes;
 public class HighlightTesting : MonoBehaviour
@@ -14,11 +21,17 @@ public class HighlightTesting : MonoBehaviour
         GridManager.SetGrid(gridSize, grid);
     }
 
+    /// <summary>
+    /// calls HiglightTilesInRange with the variables that you wanted to test
+    /// </summary>
     [Button]
     public void ShowHighlight() {
         GridManager.HiglightTilesInRange(orginTile, highlightRange, highlightColor);
     }
 
+    /// <summary>
+    /// calls GridManager.RemoveHighlight()
+    /// </summary>
     [Button]
     public void ClearHighlight()
     {
