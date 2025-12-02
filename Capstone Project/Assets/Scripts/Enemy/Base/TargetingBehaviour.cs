a71cf07d3b6579b180189a61898f8f073eeb2009
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-12-02 10:34:33 -0600

    It works but
    The player and ranged enemy need to have their colliders raised

--- Capstone Project/Assets/Scripts/Enemy/Base/TargetingBehaviour.cs
+++ Capstone Project/Assets/Scripts/Enemy/Base/TargetingBehaviour.cs
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Author: Brad Dixon
  * Creation Date: 10/20/2025
- * Last Modified: 12/01/2025
+ * Last Modified: 12/02/2025
  * Brief: Contains the different enemy targeting behaviours that are assigned
  * to enums.
  * External Resources:
@@ -23,6 +23,10 @@ public class TargetingBehaviour : MonoBehaviour
         playerPos = GridManager.playerPosition;
         RangedTargeting();
     }
+
+    [Tooltip("Layers that should be ignored for a raycast. These would be for " +
+        "objects that you wouldn't want to block enemy line of sight. Ex. Hazards")]
+    [SerializeField] private LayerMask doesNotBlockLOS;
 
     public enum TargetingBehaviours
     {
@@ -108,7 +112,7 @@ public class TargetingBehaviour : MonoBehaviour
                 if (v != playerPos)
                 {
                     //Can remove the else and have line 108 be outside the if statement, just written this way for testing purposes
-                    if (i > GetComponent<RangedEnemy>().minimumAttackDistance)
+                    if (i > GetComponent<RangedEnemy>().minimumAttackDistance && HasLineOfSight(v))
                     {
                         targetLocations.Add(v);
                         GridManager.combatGrid[v.x, v.y].entityOnGrid = 4;
@@ -136,5 +140,20 @@ public class TargetingBehaviour : MonoBehaviour
 
         GridManager.DisplayGridAsText();
         GridManager.ClearPathfinding();
+    }
+
+    private bool HasLineOfSight(Vector2Int enemyTile)
+    {
+        float yDistance = GridManager.combatGrid[enemyTile.x, enemyTile.y].gameObject.GetComponent<BoxCollider>().bounds.extents.y + .1f;
+        Vector3 tilePosition = GridManager.combatGrid[enemyTile.x, enemyTile.y].gameObject.transform.position;
+        Vector3 enemyPos = tilePosition + new Vector3(0, yDistance, 0);
+        Vector3 endTilePos = GridManager.combatGrid[GridManager.playerPosition.x, GridManager.playerPosition.y].gameObject.transform.position;
+        Vector3 endPos = endTilePos + new Vector3(0, yDistance, 0);
+        RaycastHit hit;
+        Physics.Linecast(enemyPos, endPos, out hit, ~doesNotBlockLOS);
+
+        Debug.Log(hit.collider.name);
+        Debug.Log(hit.collider.gameObject.transform.position);
+        return hit.collider.tag == "Player";
     }
 }
