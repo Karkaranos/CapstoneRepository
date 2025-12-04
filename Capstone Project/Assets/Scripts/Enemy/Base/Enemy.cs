4ae6c0ed6afadd4faee10f5082d88c7258da328d
ClareG2004 <cgrady@mail.bradley.edu>
2025-12-04 11:38:44 -0600

    im sorry

--- Capstone Project/Assets/Scripts/Enemy/Base/Enemy.cs
+++ Capstone Project/Assets/Scripts/Enemy/Base/Enemy.cs
@@ -115,7 +115,7 @@ public class Enemy : MonoBehaviour
     [SerializeField, ShowIf(nameof(currentSettings), Settings.Testing)] public TextMeshPro logText;
     [ShowIf(nameof(currentSettings), Settings.Testing), SerializeField,
         Tooltip("Time in seconds that will delay the enemy move logic after changing to move state")]
-    public int moveStateDelay;
+    public int MoveStateDelay;
 
     #endregion
 
