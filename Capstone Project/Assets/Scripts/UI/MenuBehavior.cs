369ed98dc366b8aafeeb16f44dd998e1f60f624d
Lokust001 <Lokusths@gmail.com>
2026-01-27 15:54:16 -0600

    main menu controller mapped

--- Capstone Project/Assets/Scripts/UI/MenuBehavior.cs
+++ Capstone Project/Assets/Scripts/UI/MenuBehavior.cs
@@ -6,12 +6,16 @@ Brief Description : 		Every menu gets one, these are the functions that the
                             buttons call for menu navigation
 External Resources : 
 ***************************************************/
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MenuBehavior : MonoBehaviour
 {
     [HideInInspector] public MenuBehavior previousMenu;
+    [SerializeField] private GameObject startingSelectedGO;
+    [HideInInspector] public bool isActiveMenu;
 
     /// <summary>
     /// Gets a reference to the inputActions and makes sure that the timescale is normal
@@ -19,6 +23,24 @@ public class MenuBehavior : MonoBehaviour
     private void Awake()
     {
         previousMenu = null;
+
+        
+    }
+
+    private void OnEnable()
+    {
+        isActiveMenu = true;
+        PublicEvents.ControllerEnabled += ControllerEnabled;
+
+        //if the player is using controller, selects the default obj
+        if (FindFirstObjectByType<MainMenuBehavior>().controllerEnabled)
+        {
+            ControllerEnabled();
+        }
+    }
+    private void OnDisable()
+    {
+        PublicEvents.ControllerEnabled -= ControllerEnabled;
     }
 
     /// <summary>
@@ -36,9 +58,17 @@ public class MenuBehavior : MonoBehaviour
     /// <param name="obj"></param>
     public void ActivateSubMenu(GameObject obj)
     {
+        if (FindFirstObjectByType<MainMenuBehavior>().controllerEnabled)
+        {
+            startingSelectedGO = FindFirstObjectByType<EventSystem>().currentSelectedGameObject;
+        }
+
         obj.SetActive(true);
         obj.GetComponent<MenuBehavior>().previousMenu = GetComponent<MenuBehavior>();
+       
+        isActiveMenu = false;
         gameObject.SetActive(false);
+        
     }
 
     /// <summary>
@@ -57,7 +87,31 @@ public class MenuBehavior : MonoBehaviour
         if (previousMenu != null)
         {
             previousMenu.gameObject.SetActive(true);
+            previousMenu.isActiveMenu = true;
             gameObject.SetActive(false);
         }
+    }
+
+    /// <summary>
+    /// sets the player's selected button
+    /// </summary>
+    private void ControllerEnabled()
+    {
+        if (isActiveMenu)
+        {
+            FindFirstObjectByType<EventSystem>().SetSelectedGameObject(startingSelectedGO);
+        }
+        //delays so if the menu is opened, it takes priority over the main menu
+        //StartCoroutine(DelayedControllerEnabled());
+    }
+
+    /// <summary>
+    /// sets the player's selected button with a delay
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DelayedControllerEnabled()
+    {
+        yield return null;
+       
     }
 }
