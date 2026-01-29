087fbd07178fe2c73b68eaf9979e70645d320aaa
CreamLoPuff <clout9sb@gmail.com>
2026-01-28 18:34:16 -0600

    UI SOUNDS ADDED
    There are now sounds for the tabs in the start selection menu. No Known bugs as of now. Will continue to add sounds to UI as I make more.

---
+++ Capstone Project/Assets/Scripts/Audio/UIAudioManager.cs
@@ -0,0 +1,51 @@
+/*************************************************
+Author Names : 		Gabe Holmes
+Date Created : 		01/27/2026
+Date Last Modified : 01/27/2026
+Brief Description : Basic AudioManager for UI
+External Resources : 	
+	***************************************************/
+using FMOD.Studio;
+using FMODUnity;
+using EventReference = FMODUnity.EventReference;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAudioManager : AudioManager
+{
+
+    #region VARIABLES
+    public static UIAudioManager Instance;
+
+    [SerializeField] private EventReference uiHover;
+    [SerializeField] private EventReference uiClick;
+    [SerializeField] private EventReference uiSelect;
+
+    private GameObject audioListenerObject;
+
+    #endregion VARIABLES
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #region UI_FUNCTIONS
+    public void PlayUIClick()
+    {
+        CreateEventInstance(uiClick);
+        PlayOneShot(uiClick, this.transform.position);
+    }
+    public void PlayUISelect()
+    {
+        CreateEventInstance(uiSelect);
+        PlayOneShot(uiSelect, this.transform.position);
+    }
+    public void PlayUIHover()
+    {
+        CreateEventInstance(uiHover);
+        PlayOneShot(uiHover, this.transform.position);
+    }
+    #endregion UI_FUNCTIONS
+}
