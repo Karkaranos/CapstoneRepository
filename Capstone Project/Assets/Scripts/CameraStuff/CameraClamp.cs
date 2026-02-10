f67381d7f82861ced238606184416df2c11abfcb
Sleepy2004 <aratcliffe@mail.bradley.edu>
2026-02-10 11:32:13 -0600

    Cleaning up code

--- Capstone Project/Assets/Scripts/CameraStuff/CameraClamp.cs
+++ Capstone Project/Assets/Scripts/CameraStuff/CameraClamp.cs
@@ -1,3 +1,10 @@
+/*************************************************
+Author Names : 		    Aidan Ratcliffe
+Date Created : 		    2/2/2026
+Date Last Modified : 	2/6/2026 
+Brief Description : 	This is here just in case I need to clamp the camera
+External Resources : 	N/A
+***************************************************/
 using NaughtyAttributes;
 using UnityEngine;
 public class CameraClamp : MonoBehaviour
@@ -17,10 +24,15 @@ public class CameraClamp : MonoBehaviour
     [SerializeField, ShowIf(nameof(Cams), Cameras.Refs)] Vector2 maxPos;
     #endregion
 
+    #region functions
     // Update is called once per frame
+    /// <summary>
+    /// Clamps the camera's x and y position
+    /// </summary>
     void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(CameraTarget.position.x, minPos.x, maxPos.x), 
             Mathf.Clamp(CameraTarget.position.y, minPos.y, maxPos.y), CameraTarget.position.z);
     }
+    #endregion
 }
