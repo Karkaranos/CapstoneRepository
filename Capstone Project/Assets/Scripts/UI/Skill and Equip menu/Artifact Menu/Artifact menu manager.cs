dfd49cc28633265602a39535a27261083d89f7d6
Cade Naylor <arnaylor@mail.bradley.edu>
2026-02-16 19:59:04 -0600

    Artifacts work close enough

--- Capstone Project/Assets/Scripts/UI/Skill and Equip menu/Artifact Menu/Artifact menu manager.cs
+++ Capstone Project/Assets/Scripts/UI/Skill and Equip menu/Artifact Menu/Artifact menu manager.cs
@@ -198,19 +198,28 @@ public class ArtifactMenuManager : MonoBehaviour
     {
         if (data != null)
         {
-            artifactNameText.text = data.Name;
-
-            //changes plurality depending on if theres more than one
-            if (data.ArtifactSize != 1)
+            if (artifactNameText != null)
             {
-                artifactWeightText.text = data.ArtifactSize + " Slots";
-            }
-            else
-            {
-                artifactWeightText.text = data.ArtifactSize + " Slot";
+                artifactNameText.text = data.Name;
             }
 
-            artifactDescriptionText.text = data.Description;
+            if (artifactWeightText != null)
+            {
+                //changes plurality depending on if theres more than one
+                if (data.ArtifactSize != 1)
+                {
+                    artifactWeightText.text = data.ArtifactSize + " Slots";
+                }
+                else
+                {
+                    artifactWeightText.text = data.ArtifactSize + " Slot";
+                }
+            }
+
+            if (artifactDescriptionText != null)
+            {
+                artifactDescriptionText.text = data.Description;
+            }
         }
     }
 
@@ -308,7 +317,10 @@ public class ArtifactMenuManager : MonoBehaviour
     /// </summary>
     private void UpdateCostFeedback()
     {
+        if (CostText != null)
+        {
         CostText.text = "Equipped Artifacts: " + ArtifactManager.CurrentArtifactWeight + " / " + ArtifactManager.MaxArtifactWeight;
+        }
 
 
 
