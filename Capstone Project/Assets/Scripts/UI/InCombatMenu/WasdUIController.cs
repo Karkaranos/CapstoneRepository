f33f17abd4ba02d3b7f38792a194a1887db70e7c
ClareG2004 <cgrady@mail.bradley.edu>
2026-03-05 18:59:31 -0600

    :)

--- Capstone Project/Assets/Scripts/UI/InCombatMenu/WasdUIController.cs
+++ Capstone Project/Assets/Scripts/UI/InCombatMenu/WasdUIController.cs
@@ -11,16 +11,25 @@ public class WasdUIController : MonoBehaviour
     [SerializeField] private GameObject wasdObject;
     [SerializeField] private CanvasGroup canvasGroup;
 
+    /// <summary>
+    /// Subscribe to event
+    /// </summary>
     private void OnEnable()
     {
         PublicEvents.MoveButton += ToggleWasd;
     }
 
+    /// <summary>
+    /// unsubscribe from event
+    /// </summary>
     private void OnDisable()
     {
         PublicEvents.MoveButton -= ToggleWasd;
     }
 
+    /// <summary>
+    /// Toggle if the WASD prompt is showing 
+    /// </summary>
     private void ToggleWasd()
     {
         canvasGroup.alpha = canvasGroup.alpha == 0 ? 1 : 0; 
