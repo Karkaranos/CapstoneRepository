156a1846301775a69c0e53cc5f312978815519ba
jayishembryo <kembry@mail.bradley.edu>
2026-03-26 10:48:37 -0500

    HUZZAH

--- Capstone Project/Assets/Scripts/Enemy/Base/TargetingBehaviour.cs
+++ Capstone Project/Assets/Scripts/Enemy/Base/TargetingBehaviour.cs
@@ -148,7 +148,7 @@ public class TargetingBehaviour : MonoBehaviour
         Vector3 tilePosition = GridManager.combatGrid[enemyTile.x, enemyTile.y].gameObject.transform.position;
         Vector3 enemyPos = tilePosition + new Vector3(0, yDistance, 0);
         Vector3 endTilePos = GridManager.combatGrid[GridManager.playerPosition.x, GridManager.playerPosition.y].gameObject.transform.position;
-        Vector3 endPos = endTilePos + new Vector3(0, yDistance, 0);
+        Vector3 endPos = endTilePos - new Vector3(0, yDistance, 0);
         RaycastHit hit;
         Physics.Linecast(enemyPos, endPos, out hit, ~doesNotBlockLOS);
 
