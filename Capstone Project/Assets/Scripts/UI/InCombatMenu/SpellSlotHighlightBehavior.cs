6bde3b9ad43eebe5a7f2ddd375d6a3efb0436ad2
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2026-04-07 09:55:25 -0500

    moved

---
+++ Capstone Project/Assets/Scripts/UI/InCombatMenu/SpellSlotHighlightBehavior.cs
@@ -0,0 +1,12 @@
+/*************************************************
+Author Names : 		Tyler Bouchard
+Date Created : 		4/1/2026
+Date Last Modified : 4/7/2026
+Brief Description : Highlights the spell slot when it is selected
+***************************************************/
+using UnityEngine;
+
+public class SpellSlotHighlightBehavior : MonoBehaviour
+{
+    
+}
