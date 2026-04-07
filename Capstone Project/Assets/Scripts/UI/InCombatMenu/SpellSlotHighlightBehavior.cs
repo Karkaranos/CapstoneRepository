a557559f4f698ae9cdbfcf162f56426e94f0f607
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2026-04-07 10:38:26 -0500

    Spell Slots Are Now Highlighted when Selected

--- Capstone Project/Assets/Scripts/UI/InCombatMenu/SpellSlotHighlightBehavior.cs
+++ Capstone Project/Assets/Scripts/UI/InCombatMenu/SpellSlotHighlightBehavior.cs
@@ -5,8 +5,31 @@ Date Last Modified : 4/7/2026
 Brief Description : Highlights the spell slot when it is selected
 ***************************************************/
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellSlotHighlightBehavior : MonoBehaviour
 {
-    
+    public Sprite highlightSprite;
+    public Sprite noramlSprite;
+
+    public Image slotImage;
+
+    private RuneSelectionMenu RSM;
+
+    private void Start()
+    {
+        RSM = FindAnyObjectByType<RuneSelectionMenu>();
+    }
+
+    public void showHighlight()
+    {
+        print("Callleeed");
+        RSM.RemoveAllContainerHighlights();
+        slotImage.sprite = highlightSprite;
+    }
+
+    public void RemoveHighlight()
+    {
+        slotImage.sprite = noramlSprite;
+    }
 }
