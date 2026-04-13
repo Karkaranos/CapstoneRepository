56c5061b0d786cb5013ea3486597d344f98a522a
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2026-04-13 14:58:20 -0500

    Revert "Text boxes"
    This reverts commit 5ea50e58048818952d43ba20090e0fc9e8ff2710.

--- Capstone Project/Assets/Scripts/TextBox.cs
+++ Capstone Project/Assets/Scripts/TextBox.cs
@@ -14,14 +14,10 @@ public class TextBox
 {
     [ResizableTextArea, Tooltip("What the textbox will say.")]
     public string TextboxContent;
-    [Tooltip("The size of the font.")]
-    public int FontSize;
     [Tooltip("How big the textbox should be horizontally.")]
     public float HorizontalSize;
-    [Tooltip("How big the textbox should be vertically.")]
-    public float VerticleSize;
+    [Tooltip("How big the textbox should be Vertically.")]
+    public float VerticalSize;
     [Tooltip("Where the center of the textbox will be located.")]
     public Vector2 TextboxLocation;
-    [Tooltip("If true, will show the next textbox after this.")]
-    public bool GoToNextTextbox;
 }
