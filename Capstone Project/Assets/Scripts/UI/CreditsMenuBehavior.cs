20456e0fea72549d166e26b03ea5ec3a6d37b16e
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2026-04-22 20:21:30 -0500

    credits can auto scroll now

--- Capstone Project/Assets/Scripts/UI/CreditsMenuBehavior.cs
+++ Capstone Project/Assets/Scripts/UI/CreditsMenuBehavior.cs
@@ -4,26 +4,41 @@ Date Created : 		4/21/2026
 Date Last Modified : 	4/22/2026
 Brief Description : 		controls the behavoir for the credits menu
 ***************************************************/
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CreditsMenuBehavior : MonoBehaviour
 {
-    public float topPosition;
-    public float bottomPosition;
+    public Vector3 startPos;
+    public Vector3 endPos;
 
     public RectTransform rectTransform;
     public Scrollbar scrollbar;
 
+    public float scrollDuration = 5f;
+
     private float screenLength;
+
+    public bool autoScroll;
 
     /// <summary>
     /// runs on start
     /// </summary>
     private void Start()
     {
-        screenLength = topPosition - bottomPosition;
-        topPosition = rectTransform.position.y;
+        screenLength = startPos.y - endPos.y;
+
+        if (autoScroll)
+        {
+            startPos = rectTransform.localPosition;
+            scrollbar.gameObject.SetActive(false);
+            StartCoroutine(AutoScroll());
+        }
+        else {
+            startPos = rectTransform.position;
+        }
+
     }
 
     /// <summary>
@@ -31,7 +46,24 @@ public class CreditsMenuBehavior : MonoBehaviour
     /// </summary>
     public void UpdatePagePosition()
     {
-        Vector3 newPos = new Vector3 (rectTransform.position.x, topPosition + (scrollbar.value * screenLength), rectTransform.position.z);
+        Vector3 newPos = new Vector3 (rectTransform.position.x, startPos.y - (scrollbar.value * screenLength), rectTransform.position.z);
         rectTransform.position = newPos;
+    }
+
+    private IEnumerator AutoScroll()
+    {
+        print(startPos + " " + endPos);
+        float time = 0f;
+
+        while (time < scrollDuration)
+        {
+            time += Time.deltaTime;
+
+            float t = time / scrollDuration;
+            rectTransform.localPosition = Vector3.Lerp(startPos, endPos, t);
+
+            yield return null;
+        }
+        rectTransform.localPosition = endPos;
     }
 }
