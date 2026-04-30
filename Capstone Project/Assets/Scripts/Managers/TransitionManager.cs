836891cc7a995a47dd27d4b4399d74b790a463cb
CreamLoPuff <clout9sb@gmail.com>
2026-04-30 18:54:56 -0500

    Jingle end level fix

--- Capstone Project/Assets/Scripts/Managers/TransitionManager.cs
+++ Capstone Project/Assets/Scripts/Managers/TransitionManager.cs
@@ -22,7 +22,11 @@ public class TransitionManager : MonoBehaviour
     [SerializeField] private GameObject audioListenerObject;
     [SerializeField] private EventReference EquipmnetEventRefSFX;
 
-    [SerializeField] private FMOD.Studio.Bus MasterBus;
+    [SerializeField] private FMOD.Studio.Bus masterBus;
+   [SerializeField] private FMOD.Studio.Bus bgmBus;
+    [SerializeField] private FMOD.Studio.Bus sfxBus;
+    [SerializeField] private FMOD.Studio.Bus ambBus;
+
 
 
     /// <summary>
@@ -40,7 +44,11 @@ public class TransitionManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
-        MasterBus = RuntimeManager.GetBus("Bus:/");
+
+        masterBus = RuntimeManager.GetBus("bus:/");
+        bgmBus = RuntimeManager.GetBus("bus:/BGM");
+        sfxBus = RuntimeManager.GetBus("bus:/SFX");
+        ambBus = RuntimeManager.GetBus("bus:/Ambience");
     }
 
     /// <summary>
@@ -104,7 +112,9 @@ public class TransitionManager : MonoBehaviour
     public void LevelToEndScreen()
     {
         transitionAnimator.SetTrigger("EndScreen");
-        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        bgmBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        sfxBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ambBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 
     /// <summary>
@@ -114,7 +124,9 @@ public class TransitionManager : MonoBehaviour
     {
         FindFirstObjectByType<EndLevelMenu>().ShowTheEndMenuUI();
 
-        MasterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        bgmBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        sfxBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ambBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
 
     }
 
@@ -125,6 +137,8 @@ public class TransitionManager : MonoBehaviour
     {
         transitionAnimator.SetTrigger("EquipMenu");
         FindFirstObjectByType<SkillAndArtifactManager>(FindObjectsInactive.Include)?.SetButton();
+
+        masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
 
         AudioManager.instance.CreateEventInstance(EquipmnetEventRefSFX);
         AudioManager.instance.PlayOneShot(EquipmnetEventRefSFX, audioListenerObject.transform.position);
